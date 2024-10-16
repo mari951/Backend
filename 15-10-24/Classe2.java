ed4f9e59b8981d6fcbff2ad4731fd394857fab5d
mari951 <marieaz843@gmail.com>
2024-10-16 11:35:29 -0300

    Add files via upload

--- 15-10-24/Classe2.java
+++ 15-10-24/Classe2.java
@@ -1,3 +1,14 @@
 public class Classe2 {
+public static int calcularSoma(int numero1,int numero2) {
+return numero1+numero2;
+}
+public static String [] mostrarOpcoes(){
+
+    {
+    
+public static String resultado(int intInputUsuario1,intInputUsuario2,int opcaoEscolhida) {
+    return Classe3.processarDados(intInputUsuario1,intInputUsuario2,opcaoEscolhida);
+}
+
     
 }
