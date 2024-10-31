cfe717ab4f58c788dd67db2f54dce4d7f8c4dd27
mari951 <marieaz843@gmail.com>
2024-10-31 09:55:06 -0300

    Add files via upload

---
+++ TelaDeLoginModel e Controller (4).java
@@ -0,0 +1,24 @@
+package model;
+import java.sql.*;
+import controller.*;
+
+public class TelaDeLoginModel extends TelaDeLoginController {
+    public static void logarModel(String login, String senha) {
+        try {
+            Connection conexao = MySQLConnector.conectar();
+            String strSqlLogin = "select * from `db_senac`.`tbl_senac` where `email` = '" + login + "' and `senha` = '" + senha + "';";
+            Statement stmSqlLogin = conexao.createStatement();
+            ResultSet rstSqlLogin = stmSqlLogin.executeQuery(strSqlLogin);
+            if (rstSqlLogin.next()) {
+                TelaDeLoginController.notificarUsuario("Login realizado com sucesso!!!");
+                TelaDeLoginController.abrirTelaDeMenu();
+            } else {
+                TelaDeLoginController.notificarUsuario("Login e/ou senha não encontrados! Por favor, verifique e tente novamente.");
+            }
+            stmSqlLogin.close();
+        } catch (Exception e) {
+            TelaDeLoginController.notificarUsuario("Houve um problema e não será possível realizar o login agora. Por favor, tente novamente mais tarde.");
+            System.err.println("Veja o erro: " + e);
+        }
+    }
+}
